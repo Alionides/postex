876838be55f409ea741fd522577b99981679aef6
Alionides <Alionides>
2022-02-11 13:15:48 +0400

    customer id fix

--- app/Models/Acceptance.php
+++ app/Models/Acceptance.php
@@ -10,6 +10,7 @@ class Acceptance extends Model
     use HasFactory;
 
     protected $fillable = [
+        'customer_id',
         'sender_fin',
         'sender_first_name',
         'sender_last_name',
@@ -26,9 +27,9 @@ class Acceptance extends Model
         'barcodes',
         'status'
     ];
+}
 
     public function customer()
     {
         return $this->belongsTo(Customer::class);
     }
-}
